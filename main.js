663726b931f275e34fe01200a21f1c0a551ff922
slavajamm <kvaracheliyaRAM@gmail.com>
2018-06-27 16:40:19 +0300

    Vers 1.0.1

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
-$('.start-page--click').click(function(){
-	$('.start-page').remove();
-	$('.change-time').show();
-})
+(function($) {
+	//var clickedBirds = 0;
+	
+	var animObj = {
+		clickedBirds: 0,
+		startPageObj: $('.start-page'),
 
-var audio = document.getElementsByTagName('audio')[0];
+		_init: function() {
+			var self = animObj;
 
-var path = document.getElementsByTagName('path');
+			console.log('init');
+			$(".start-page--click").click(self._startPageClick);
+			$("path").mouseenter(self._addAudio);
+			$('.change-time').click(self._changeTime);
+			$('svg path').click(self._removeBird);
+		},
+		_startPageClick: function() {
+			var self = animObj;
 
-$("path").mouseenter(function(){
-	$("<audio></audio>").attr({ 
-		'src':'sound/'+Math.ceil(Math.random() * 5)+'.wav', 
-		'volume':0.4,
-		'autoplay':'autoplay'
-	}).appendTo("body");
-});
-//at the day
-$('.change-time').click(function(){
-	$(this).toggleClass('--night');
-	$('body').toggleClass('--night');
-});
-//remove birds
-	var clickedBirds = 0;
+			self.startPageObj.remove();
+			$('.change-time').show();
+		},
+		_addAudio: function() {
+			$("<audio></audio>").attr({ 
+				'src': 'sound/' + Math.ceil(Math.random() * 5) + '.wav', 
+				'volume': 0.4,
+				'autoplay': 'autoplay'
+			}).appendTo("body");
+		},
+		_changeTime: function() {
+			$(this).toggleClass('--night');
+			$('body').toggleClass('--night');
+		},
+		_removeBird: function() {
+			var self = animObj;
 
-$('svg path').click(function(){
-	$(this).remove();
-	clickedBirds += 1;
+			$(this).remove();
+			self.clickedBirds += 1;
+			$('#crowsClicked').html(self.clickedBirds);
+		}
+	};
+	
 
-	$('#crowsClicked').html(clickedBirds);
+	$(document).ready(animObj._init);
 
-});
+	console.log("IIF");
+})(jQuery);
